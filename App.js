9ecf6846a0e608b555e1bfe3772cbfdf7a63fa24
LzxHahaha <lzxglhf@live.com>
2018-12-29 21:11:56 +0800

    use uncontrol input

--- App.js
+++ App.js
@@ -2,19 +2,23 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default class App extends Component {
+  value = '';
   state = {
-    value: '',
     display: ''
   };
 
-  onChangeText = value => this.setState({ value });
-  onSubmit = () => this.setState({ display: this.state.value, value: '' });
+  onChangeText = value => this.value = value;
+  onSubmit = () => {
+    this.setState({ display: this.value });
+    this.input.clear();
+    this.value = '';
+  };
 
   render() {
     return (
       <View style={styles.container}>
         <TextInput
-          value={this.state.value}
+          ref={ref => this.input = ref}
           autoFocus
           blurOnSubmit={false}
           underlineColorAndroid="#AAA"
